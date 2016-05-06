2d680cff4f9e54db7e2193c6ede7e8340ac6ff95
Giordon Stark <kratsg@gmail.com>
2016-05-05 23:24:27 -0500

    clean up run script more

--- Gbb/run.sh
+++ Gbb/run.sh
@@ -1,2 +1,8 @@
-python ../Optimization/optimize.py cut -b --numpy --ncores 7 $(find ~/hf_tag02_v1/*dijet*.merged ! -name "*Gtt*" -type f) --tree nominal --eventWeight weight_mc --weightsFile ../Optimization/weights.yml -h
-python ../Optimization/optimize.py optimize --signal 37* --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') -b --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2 --lumi 10
+# produce cuts for all the samples
+python ../optimize.py cut -b --numpy --ncores 7 \
+    $(find ~/hf_tag02_v1/*.merged ! -name "*Gtt*" -type f) \
+    --tree nominal --eventWeight weight_mc --weightsFile ../weights.yml
+# calculate significances with lumi = 10 ifb
+python ../Optimization/optimize.py optimize --signal 37* \
+    --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') \
+    -b --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2 --lumi 10
