f5dd0c33eee4dfe2e423e729cdb2961a6964953e
Giordon Stark <kratsg@gmail.com>
2016-05-05 23:11:10 -0500

    update the paths so the run makes more sense

--- Gbb/run.sh
+++ Gbb/run.sh
@@ -1,2 +1,2 @@
-python ../optimize.py cut -b --numpy --ncores 7 $(find ~/hf_tag02_v1/*dijet*.merged ! -name "*Gtt*" -type f) --tree nominal --eventWeight weight_mc --weightsFile ../weights.yml -h
-python ../optimize.py optimize --signal 37* --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') -b --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2 --lumi 10
+python ../Optimization/optimize.py cut -b --numpy --ncores 7 $(find ~/hf_tag02_v1/*dijet*.merged ! -name "*Gtt*" -type f) --tree nominal --eventWeight weight_mc --weightsFile ../Optimization/weights.yml -h
+python ../Optimization/optimize.py optimize --signal 37* --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') -b --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2 --lumi 10
