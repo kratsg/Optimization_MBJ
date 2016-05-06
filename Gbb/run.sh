0adb1fdb9a2177498f15c6568d5df9542af9716f
Giordon Stark <kratsg@gmail.com>
2016-05-06 14:29:54 -0500

    bump version and add the graph-grid script

--- Gbb/run.sh
+++ Gbb/run.sh
@@ -6,3 +6,7 @@ python ../optimize.py cut -b --numpy --ncores 7 \
 python ../Optimization/optimize.py optimize --signal 37* \
     --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') \
     -b --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2 --lumi 10
+# produce the sig plot
+python ../Optimization/graph-grid.py --lumi 10 --outfile plots \
+    --sigdir significances --cutdir cuts --massWindows ../massWindows_Gbb.txt \
+    --run1_csvfile ../Optimization/run1_limit.csv --run1_1sigma_csvfile ../Optimization/run1_limit_1sigma.csv
