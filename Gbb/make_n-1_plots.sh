4be65ccd3c0dd9a21dcf8bd57d13420a039cd7b8
Giordon Stark <kratsg@gmail.com>
2016-05-23 23:19:35 +0200

    add n-1 plots for VR0L and VR1L

--- Gbb/make_n-1_plots.sh
+++ Gbb/make_n-1_plots.sh
@@ -2,3 +2,5 @@
 
 python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_SR.yml --weights TOTW/weights.yml -i n-1
 python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_CR.yml --weights TOTW/weights.yml -i n-1
+python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_VR0L.yml --weights TOTW/weights.yml -i n-1
+python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_VR1L.yml --weights TOTW/weights.yml -i n-1
