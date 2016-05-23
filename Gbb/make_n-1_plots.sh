7a4bedf984cb323575daf592da1e45a2f1ad1a40
Giordon Stark <kratsg@gmail.com>
2016-05-23 15:55:02 +0200

    add TOTW CR n-1 as well

--- Gbb/make_n-1_plots.sh
+++ Gbb/make_n-1_plots.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_SR.yml --weights TOTW/weights.yml -i n-1
+python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_CR.yml --weights TOTW/weights.yml -i n-1
