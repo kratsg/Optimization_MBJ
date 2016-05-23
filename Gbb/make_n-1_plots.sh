5d89096fc7b165e6466b275b163b34c8aaaf3ef0
Giordon Stark <kratsg@gmail.com>
2016-05-23 11:53:54 +0200

    update path to weights file

--- Gbb/make_n-1_plots.sh
+++ Gbb/make_n-1_plots.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_SR.yml --weights ~/TakeOverTheWorld/weights.yml -i n-1
+python ~/TakeOverTheWorld/totw.py -b --config TOTW/n-1_SR.yml --weights TOTW/weights.yml -i n-1
