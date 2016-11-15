d238761f0e2beadbc1e5c36d8d1c3c0ef20a071a
Giordon Stark <kratsg@gmail.com>
2016-11-15 14:34:17 -0600

    update scripts for the appropriate paths

--- PAR2016/cuts.sh
+++ PAR2016/cuts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts_normal.json -o cuts_normal --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
-python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts_jigsaw.json -o cuts_jigsaw --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
+python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts/normal.json -o cuts/normal --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
+python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts/jigsaw.json -o cuts/jigsaw --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
--- PAR2016/optimize.sh
+++ PAR2016/optimize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts_normal -b --o=significances_normal --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
-python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts_jigsaw -b --o=significances_jigsaw --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
+python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts/normal -b --o=significances/normal --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
+python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts/jigsaw -b --o=significances/jigsaw --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
