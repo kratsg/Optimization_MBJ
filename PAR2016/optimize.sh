0d48d415afcf93617b0081a7f7ebbf39ca8a2c1e
Giordon Stark <kratsg@gmail.com>
2016-11-15 19:59:25 -0600

    add bkgdFiles to optimization

--- PAR2016/optimize.sh
+++ PAR2016/optimize.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts/normal -b --o=significances/normal --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
-python ../Optimization/optimize.py optimize --signal 37* --bkgd 4* --searchDirectory=cuts/jigsaw -b --o=significances/jigsaw --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
+python ../Optimization/optimize.py optimize --signal 37* --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') --searchDirectory=cuts/normal -b --o=significances/normal --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
+python ../Optimization/optimize.py optimize --signal 37* --bkgd $(cat bkgdFiles | sed "s/$/.json/g" | tr '\n' ' ') --searchDirectory=cuts/jigsaw -b --o=significances/jigsaw --bkgdUncertainty=0.3 --bkgdStatUncertainty=0.3 --insignificance=2
