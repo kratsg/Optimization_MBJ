07997b91eea8dd2257823d9847e63838fbf08633
Giordon Stark <kratsg@gmail.com>
2016-11-19 00:03:29 -0600

    exclude Gbb and data

--- PAR2016/cuts.sh
+++ PAR2016/cuts.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts/normal_${version}.json -o cuts/normal_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
-python ../Optimization/optimize.py cut ~/2.4.19-0-0/*.root --supercuts=supercuts/jigsaw_${version}.json -o cuts/jigsaw_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
+files=$(find /Users/kratsg/2.4.19-0-0/*.root  -not -iname "*Gbb*" -not -iname "*data*")
+
+python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/normal_${version}.json -o cuts/normal_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
+python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/jigsaw_${version}.json -o cuts/jigsaw_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
