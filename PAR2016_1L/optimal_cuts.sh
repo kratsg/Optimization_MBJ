01389d0c5eab74a72084c2b3446d8fe767685820
Giordon Stark <kratsg@gmail.com>
2016-11-24 08:49:28 -0600

    for now only normal

--- PAR2016_1L/optimal_cuts.sh
+++ PAR2016_1L/optimal_cuts.sh
@@ -3,5 +3,5 @@
 python ../Optimization/optimize.py summary --searchDirectory significances/normal_${version} --massWindows ../massWindows_Gtt.txt --output summary_normal_${version}.json
 python ../Optimization/optimize.py hash summary_normal_${version}.json --supercuts supercuts/normal_${version}.json -o outputHash/normal_${version} --use-summary
 
-python ../Optimization/optimize.py summary --searchDirectory significances/jigsaw_${version} --massWindows ../massWindows_Gtt.txt --output summary_jigsaw_${version}.json
-python ../Optimization/optimize.py hash summary_jigsaw_${version}.json --supercuts supercuts/jigsaw_${version}.json -o outputHash/jigsaw_${version} --use-summary
+#python ../Optimization/optimize.py summary --searchDirectory significances/jigsaw_${version} --massWindows ../massWindows_Gtt.txt --output summary_jigsaw_${version}.json
+#python ../Optimization/optimize.py hash summary_jigsaw_${version}.json --supercuts supercuts/jigsaw_${version}.json -o outputHash/jigsaw_${version} --use-summary
