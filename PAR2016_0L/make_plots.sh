1f3a00c0dd63f7716db84b5fddf34eafe8fee5bc
Giordon Stark <kratsg@gmail.com>
2016-11-24 23:14:52 -0600

    update make_plots.sh

--- PAR2016_0L/make_plots.sh
+++ PAR2016_0L/make_plots.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-python ../Optimization/graph-grid.py --lumi 35 --outfile normal_${version} --cutdir cuts/normal_${version} --sigdir significances/normal_${version} --massWindows ../massWindows_Gtt.txt --run1_csvfile ../Optimization/run1_limit.csv --run1_1sigma_csvfile ../Optimization/run1_limit_1sigma.csv
-python ../Optimization/graph-grid.py --lumi 35 --outfile jigsaw_${version} --cutdir cuts/jigsaw_${version} --sigdir significances/jigsaw_${version} --massWindows ../massWindows_Gtt.txt --run1_csvfile ../Optimization/run1_limit.csv --run1_1sigma_csvfile ../Optimization/run1_limit_1sigma.csv
+python ../Optimization/graph-grid.py --summary summary_normal_${version}.json --lumi 35 -o normal_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
+python ../Optimization/graph-grid.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
 
-python ../Optimization/graph-cuts.py --lumi 35 --outfile normal_${version} --supercuts supercuts/normal_${version}.json --sigdir significances/normal_${version} --hashdir outputHash/normal_${version} --massWindows ../massWindows_Gtt.txt
-python ../Optimization/graph-cuts.py --lumi 35 --outfile jigsaw_${version} --supercuts supercuts/jigsaw_${version}.json --sigdir significances/jigsaw_${version} --hashdir outputHash/jigsaw_${version} --massWindows ../massWindows_Gtt.txt
+python ../Optimization/graph-cuts.py --summary summary_normal_${version}.json --lumi 35 -o normal_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/normal_${version} --supercuts supercuts/normal_${version}.json
+python ../Optimization/graph-cuts.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/jigsaw_${version} --supercuts supercuts/jigsaw_${version}.json
+
