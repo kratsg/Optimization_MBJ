3ff5a2bf2d887e5c3a9ee0eef88b20e238c315ea
Giordon Stark <kratsg@gmail.com>
2016-11-29 13:52:21 -0600

    update the scripts to only look at normal for now

--- PAR2016_0L/cuts.sh
+++ PAR2016_0L/cuts.sh
@@ -2,5 +2,5 @@
 
 files=$(find /Users/kratsg/2.4.19-0-0/*.root  -not -iname "*Gbb*" -not -iname "*data*")
 
-python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/normal_${version}.json -o cuts/normal_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
-python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/jigsaw_${version}.json -o cuts/jigsaw_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
+python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/normal_${version}.json -o cuts/normal_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree --ncores=7
+#python ../Optimization/optimize.py cut ${files} --supercuts=supercuts/jigsaw_${version}.json -o cuts/jigsaw_${version} --numpy -b --eventWeight "event_weight" --weightsFile ../Optimization/weights.json --tree oTree
--- PAR2016_0L/make_plots.sh
+++ PAR2016_0L/make_plots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python ../Optimization/graph-grid.py --summary summary_normal_${version}.json --lumi 35 -o normal_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
-python ../Optimization/graph-grid.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
+#python ../Optimization/graph-grid.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b
 
 python ../Optimization/graph-cuts.py --summary summary_normal_${version}.json --lumi 35 -o normal_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/normal_${version} --supercuts supercuts/normal_${version}.json
-python ../Optimization/graph-cuts.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/jigsaw_${version} --supercuts supercuts/jigsaw_${version}.json
+#python ../Optimization/graph-cuts.py --summary summary_jigsaw_${version}.json --lumi 35 -o jigsaw_${version} --do-run1 --run1-excl ../Optimization/run1_limit.csv --run1-1sigma ../Optimization/run1_limit_1sigma.csv -b --outputHash outputHash/jigsaw_${version} --supercuts supercuts/jigsaw_${version}.json
 
