98e0600c018be2ce39c4103cbdf6a8b246c6c1b5
Giordon Stark <kratsg@gmail.com>
2017-01-08 15:22:21 -0600

    whoops typo

--- Moriond2017/make_comparison.sh
+++ Moriond2017/make_comparison.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ../Optimization/summary-comparison.py --base-summary summary_baseline_v1.json --comp-summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} -b
+python ../Optimization/summary-comparison.py --base-summary summary_baseline_0L.json --comp-summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} -b
