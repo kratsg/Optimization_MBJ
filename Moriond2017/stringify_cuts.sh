34874b820c5522791c1f32f16293f6473017a38f
Giordon Stark <kratsg@gmail.com>
2017-01-10 13:30:53 -0600

    a way to stringify cuts for this

---
+++ Moriond2017/stringify_cuts.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for f in $(find regions_${version}/*.json)
+do
+  filename=$(basename "$f")
+  extension="${f##*.}"
+  filename="${filename%##*/}"
+  filename="${filename%.*}"
+
+
+  python -c "import sys, os; sys.path.append('../Optimization'); from root_optimize.utils import cuts_to_selection; import json; print('$filename    '+cuts_to_selection(json.load(file('$f','r'))))" | grep $filename
+
+done
