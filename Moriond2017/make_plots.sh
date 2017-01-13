4e7d4845eddf2828eba2ad7979dd1fbd537a7585
Giordon Stark <kratsg@gmail.com>
2017-01-12 22:14:04 -0600

    switch to run2

--- Moriond2017/make_plots.sh
+++ Moriond2017/make_plots.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-python ../Optimization/graph-grid.py --summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} --do-run1 --run1-excl ../run1_limit.csv --run1-1sigma ../run1_limit_1sigma.csv -b
-python ../Optimization/graph-cuts.py --summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} --do-run1 --run1-excl ../run1_limit.csv --run1-1sigma ../run1_limit_1sigma.csv -b --outputHash outputHash/baseline_${version} --supercuts supercuts/baseline_${version}.json
+python ../Optimization/graph-grid.py --summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} --do-run2 --run2-excl ../run2_limit.csv --run2-1sigma ../run2_limit_1sigma.csv -b
+python ../Optimization/graph-cuts.py --summary summary_baseline_${version}.json --lumi 35 -o baseline_${version} -b --outputHash outputHash/baseline_${version} --supercuts supercuts/baseline_${version}.json
 
-python ../Optimization/graph-grid.py --summary summary_baseline_${version}_ttbarScaled.json --lumi 35 -o baseline_${version}_ttbarScaled --do-run1 --run1-excl ../run1_limit.csv --run1-1sigma ../run1_limit_1sigma.csv -b
-python ../Optimization/graph-cuts.py --summary summary_baseline_${version}_ttbarScaled.json --lumi 35 -o baseline_${version}_ttbarScaled --do-run1 --run1-excl ../run1_limit.csv --run1-1sigma ../run1_limit_1sigma.csv -b --outputHash outputHash_ttbarScaled/baseline_${version} --supercuts supercuts/baseline_${version}.json
+python ../Optimization/graph-grid.py --summary summary_baseline_${version}_ttbarScaled.json --lumi 35 -o baseline_${version}_ttbarScaled --do-run2 --run2-excl ../run2_limit.csv --run2-1sigma ../run2_limit_1sigma.csv -b
+python ../Optimization/graph-cuts.py --summary summary_baseline_${version}_ttbarScaled.json --lumi 35 -o baseline_${version}_ttbarScaled -b --outputHash outputHash_ttbarScaled/baseline_${version} --supercuts supercuts/baseline_${version}.json
 
